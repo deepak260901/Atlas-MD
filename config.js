bed757eccbf9675754fc5116f3934442289d64cb
deepak260901 <105791403+deepak260901@users.noreply.github.com>
2023-03-16 18:16:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "919015286428";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
@@ -44,8 +44,8 @@ global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `『ɪɴᴅ』☯▓🌛🎗🎗🅿🅰🎋𓀎`;
+global.author = process.env.AUTHOR || "Gappu(🪔)🤗💫";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
